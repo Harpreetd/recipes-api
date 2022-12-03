07d14fee9c238d117e5abe264eae0757d98fb199
Harpreetd <dhimanharpreet17@yahoo.in>
2022-12-04 00:22:22 +0100

    multiple foreign keys not working

--- createTable.js
+++ createTable.js
@@ -3,13 +3,21 @@
 
 const sqlite = require("sqlite3").verbose();
 const db = new sqlite.Database(
-  __dirname + "./recipe.db",
+  __dirname + "./recipeCollection.db",
 
   (err) => {
     if (err) return console.error(err);
   }
 );
 
-const sql =
-  "CREATE TABLE IF NOT EXISTS recipe(id INTEGER PRIMARY KEY AUTOINCREMENT,recipeName TEXT, recipeCategory TEXT)";
-db.run(sql);
+// const sql =
+//   "CREATE TABLE IF NOT EXISTS RecipesList(recipeId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,recipeName TEXT, recipeCategory TEXT)";
+// db.run(sql);
+
+// const sql =
+//   "CREATE TABLE IF NOT EXISTS IngredientsList(ingredientId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,ingredientName Text)";
+// db.run(sql);
+
+// const sql =
+// " CREATE TABLE IF NOT EXISTS RecipeIngredients(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,recipeId INT, FOREIGN KEY (recipeId) REFERENCES RecipesList(recipeId))";
+// db.run(sql);
--- index.js
+++ index.js
@@ -1,16 +1,67 @@
-const express = require("express"); // importing express from express module for request processing
-const bodyParser = require("body-parser"); // importing body-parser
-const hostname = "localhost"; // name of the server
-const port = process.env.PORT || 8080; // port of the server
-const app = express(); // creating an express app instance
-app.use(bodyParser.json()); //using body-parser to parse the request body
+// const express = require("express"); // importing express from express module for request processing
+// const bodyParser = require("body-parser"); // importing body-parser
+// const hostname = "localhost"; // name of the server
+// const port = process.env.PORT || 8080; // port of the server
+// const app = express(); // creating an express app instance
+// app.use(bodyParser.json()); //using body-parser to parse the request body
 
-const sqlite = require("sqlite3").verbose();
-const db = new sqlite.Database(__dirname + "./recipe.db", (err) => {
-  if (err) return console.error(err);
-});
+// // connecting to the database
+// const sqlite = require("sqlite3").verbose();
+// const db = new sqlite.Database(__dirname + "./recipe.db", (err) => {
+//   if (err) return console.error(err);
+//   console.log("connection created");
+// });
+// let sql;
+// // just for testing
+// app.get("/", (req, res) => {
+//   return res.json({
+//     recipeName: "pancakes",
+//     recipeCategory: "free",
+//   });
+//   res.end();
+// });
 
-// Server listening
-app.listen(port, () => {
-  console.log(`server is running at http://${hostname}:${port}`);
-});
+// // get all recipes
+// app.get("/ingredient", (req, res) => {
+//   sql = "SELECT * FROM ingredient";
+//   try {
+//     db.run(sql, [], (err, rows) => {
+//       console.log(rows);
+//       if (err) return res.json({ status: 300, success: false, error: err });
+//       // if (rows.length < 1)
+//       //   return res.json({
+//       //     status: 300,
+//       //     success: false,
+//       //     error: "no match found",
+//       //   });
+//       return res.json({ status: 200, data: rows, success: true });
+//     });
+//   } catch (error) {
+//     return res.json({
+//       status: 404,
+//       error: error,
+//       success: false,
+//     });
+//   }
+// });
+
+// // app.post("/recipe", (req, res) => {
+// //   try {
+// //     const { recipeName, recipeCategory } = req.body;
+// //     (sql = "INSERT INTO recipes  VALUES(recipeName,recipeCategory)"),
+// //       recipeName,
+// //       recipeCategory;
+// //     db.run(sql);
+// //   } catch (error) {
+// //     return res.json({
+// //       status: 404,
+// //       success: false,
+// //       error: error,
+// //     });
+// //   }
+// // });
+
+// // Server listening
+// app.listen(port, () => {
+//   console.log(`server is running at http://${hostname}:${port}`);
+// });
