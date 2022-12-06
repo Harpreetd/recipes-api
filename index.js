710ceb393b1e85b76816c4d2590a590eade9100c
Harpreetd <dhimanharpreet17@yahoo.in>
2022-12-06 01:00:43 +0100

    getting free or premium recipes with cookie jar on or off on postman

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express"); // importing express from express module for request processing
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const sessions = require("express-session");
 const bodyParser = require("body-parser"); // importing body-parser
 const hostname = "localhost"; // name of the server
 const port = process.env.PORT || 8080; // port of the server
@@ -9,21 +11,29 @@ app.use(morgan("dev"));
 app.use(bodyParser.json()); //using body-parser to parse the request body
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  sessions({
+    secret: "food monster",
+    saveUninitialized: true,
+    cookie: { maxAge: 6000 },
+    resave: true,
+    secure: false,
+  })
+);
+
 // connecting to the database
 const sqlite = require("sqlite3").verbose();
 const db = new sqlite.Database(__dirname + "./recipeCollection.db", (err) => {
   if (err) return console.error(err);
   console.log("connection created");
 });
-let sql;
+// let sql;
 // just for testing ---working
-// app.get("/", (req, res) => {
-//   return res.json({
-//     recipeName: "pancakes",
-//     recipeCategory: "free",
-//   });
-//   res.end();
-// });
+app.get("/", (req, res) => {
+  res.cookie("usertype", "premium");
+  res.end();
+});
 
 // get all recipes
 // app.get("/recipe", (req, res) => {
@@ -55,24 +65,37 @@ let sql;
 
 // get all recipes
 app.get("/recipe", (req, res) => {
-  console.log("usertype", req.params);
+  console.log(req.cookies.usertype);
   let data = [];
-  db.serialize(() => {
-    db.each(
-      "SELECT * FROM Recipes WHERE category='Free';",
-      (err, row) => {
-        if (err) return res.json({ status: 300, success: false, error: err });
-        data.push(row);
-      },
-      () => {
-        res.send(data);
-      }
-    );
-  });
+  if (req.cookies.usertype === "premium") {
+    db.serialize(() => {
+      db.each(
+        "SELECT * FROM Recipes WHERE category='Premium';",
+        (err, row) => {
+          if (err) return res.json({ status: 300, success: false, error: err });
+          data.push(row);
+        },
+        () => {
+          res.send(data);
+        }
+      );
+    });
+  } else {
+    db.serialize(() => {
+      db.each(
+        "SELECT * FROM Recipes WHERE category='Free';",
+        (err, row) => {
+          if (err) return res.json({ status: 300, success: false, error: err });
+          data.push(row);
+        },
+        () => {
+          res.send(data);
+        }
+      );
+    });
+  }
 });
-//  `SELECT * FROM Recipes WHERE recipe_Id= ${recipeId};`,
-// const getRecipeIngredients =
-//   "SELECT r.recipe_Name, i.ingredient_Name from Recipes r inner JOIN RecipeIngredients t on r.recipe_Id= t.recipe_Id inner join Ingredients i on t.ingredient_Id = i.ingredient_Id;";
+
 // Get a particular recipe
 app.get("/recipe/:recipe_Id", (req, res) => {
   let recipeId = req.params.recipe_Id;
