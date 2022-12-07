8b7cf616cdf3fa5f62a976e25d0f7f276e0d2421
Harpreetd <dhimanharpreet17@yahoo.in>
2022-12-07 22:39:56 +0100

    writing in response code and errors- not complete yet

--- index.js
+++ index.js
@@ -65,9 +65,8 @@ app.get("/", (req, res) => {
 
 // get all recipes
 app.get("/recipe", (req, res) => {
-  // console.log(req.cookies.usertype);
   let data = [];
-  if (req.cookies.usertype === "premium") {
+  if (req.cookies.usertype === "premium" || req.cookies.usertype === "admin") {
     db.serialize(() => {
       db.each(
         "SELECT * FROM Recipes;",
@@ -89,7 +88,13 @@ app.get("/recipe", (req, res) => {
           data.push(row);
         },
         () => {
-          res.send(data);
+          if (data.length < 1)
+            return res.json({
+              status: 404,
+              success: false,
+              error: "No recipes Found, try again later.",
+            });
+          return res.json({ status: 200, data: data, success: true });
         }
       );
     });
@@ -104,68 +109,147 @@ app.get("/recipe/:recipe_Id", (req, res) => {
   let recipeName;
   let stepsCount;
   let ingredients = [];
-  db.serialize(() => {
-    db.each(
-      `SELECT r.recipe_Name,r.step_Count, i.ingredient_Type, m.measure from Recipes r inner JOIN RecipeIngredients t on r.recipe_Id= t.recipe_Id inner join Ingredients i on t.ingredient_Id = i.ingredient_Id inner JOIN Measurements m on m.measure_Id=t.measure_Id WHERE r.recipe_Id=?;`,
-      recipeId,
-      (err, row) => {
-        if (err) return res.json({ status: 300, success: false, error: err });
+  if (req.cookies.usertype === "premium" || req.cookies.usertype === "admin") {
+    db.serialize(() => {
+      db.each(
+        `SELECT r.recipe_Name,r.step_Count, i.ingredient_Type, m.measure from Recipes r inner JOIN RecipeIngredients t on r.recipe_Id= t.recipe_Id inner join Ingredients i on t.ingredient_Id = i.ingredient_Id inner JOIN Measurements m on m.measure_Id=t.measure_Id WHERE r.recipe_Id=?;`,
+        recipeId,
+        (err, row) => {
+          if (err) return res.json({ status: 300, success: false, error: err });
 
-        recipeName = row.recipe_Name;
-        stepsCount = row.step_Count;
-        let measureText = row.measure;
-        let ingredientType = row.ingredient_Type;
-        let ingredient = { type: ingredientType, amount: measureText };
-        ingredients.push(ingredient);
-        data = {
-          recipeName: recipeName,
-          ingredients: [...ingredients],
-          stepCount: stepsCount,
-        };
-      },
-      () => {
-        res.send(data);
-      }
-    );
-  });
+          recipeName = row.recipe_Name;
+          stepsCount = row.step_Count;
+          let measureText = row.measure;
+          let ingredientType = row.ingredient_Type;
+          let ingredient = { type: ingredientType, amount: measureText };
+          ingredients.push(ingredient);
+          data = {
+            recipeName: recipeName,
+            ingredients: [...ingredients],
+            stepCount: stepsCount,
+          };
+        },
+        () => {
+          res.send(data);
+        }
+      );
+    });
+  } else {
+    db.serialize(() => {
+      db.each(
+        `SELECT r.recipe_Name,r.step_Count, i.ingredient_Type, m.measure from Recipes r inner JOIN RecipeIngredients t on r.recipe_Id= t.recipe_Id inner join Ingredients i on t.ingredient_Id = i.ingredient_Id inner JOIN Measurements m on m.measure_Id=t.measure_Id WHERE r.recipe_Id=? AND r.category='Free';`,
+        recipeId,
+        (err, row) => {
+          if (err) return res.json({ status: 300, success: false, error: err });
+
+          recipeName = row.recipe_Name;
+          stepsCount = row.step_Count;
+          let measureText = row.measure;
+          let ingredientType = row.ingredient_Type;
+          let ingredient = { type: ingredientType, amount: measureText };
+          ingredients.push(ingredient);
+          data = {
+            recipeName: recipeName,
+            ingredients: [...ingredients],
+            stepCount: stepsCount,
+          };
+        },
+        () => {
+          if (!data)
+            return res.json({
+              status: 401,
+              success: false,
+              error: "Unauthorized user, get a premium user token to continue",
+            });
+          return res.json({ status: 200, data: data, success: true });
+        }
+      );
+    });
+  }
 });
 
 // get detailed steps of a given recipe
 app.get("/recipe/:recipe_Id/all", (req, res) => {
   let recipeId = req.params.recipe_Id;
   let data = [];
-  db.serialize(() => {
-    db.each(
-      `SELECT s.step_detail,s.step_Id FROM Steps s INNER JOIN Recipes r ON r.recipe_Id=s.recipe_Id WHERE r.recipe_Id=${recipeId};`,
-      (err, row) => {
-        if (err) return res.json({ status: 300, success: false, error: err });
-        data.push({ stepId: row.step_Id, text: row.step_detail });
-      },
-      () => {
-        res.send(data);
-      }
-    );
-  });
+  if (req.cookies.usertype === "premium" || req.cookies.usertype === "admin") {
+    db.serialize(() => {
+      db.each(
+        `SELECT s.step_detail,s.step_Id FROM Steps s INNER JOIN Recipes r ON r.recipe_Id=s.recipe_Id WHERE r.recipe_Id=(?);`,
+        recipeId,
+        (err, row) => {
+          if (err) return res.json({ status: 300, success: false, error: err });
+          data.push({ stepId: row.step_Id, text: row.step_detail });
+        },
+        () => {
+          res.send(data);
+        }
+      );
+    });
+  } else {
+    db.serialize(() => {
+      db.each(
+        `SELECT s.step_detail,s.step_Id FROM Steps s INNER JOIN Recipes r ON r.recipe_Id=s.recipe_Id WHERE r.recipe_Id=(?) AND r.category="Free";`,
+        recipeId,
+        (err, row) => {
+          if (err) return res.json({ status: 300, success: false, error: err });
+          data.push({ stepId: row.step_Id, text: row.step_detail });
+        },
+        () => {
+          if (data.length < 1)
+            return res.json({
+              status: 401,
+              success: false,
+              error: "Unauthorized user, get a premium user token to continue",
+            });
+          return res.json({ status: 200, data: data, success: true });
+        }
+      );
+    });
+  }
 });
 // get a particular step of a particular recipe
 app.get("/recipe/:recipe_Id/:step_Id", (req, res) => {
   let recipeId = req.params.recipe_Id;
   let stepId = req.params.step_Id;
   let data = [];
-  db.serialize(() => {
-    db.each(
-      `SELECT s.step_detail,s.step_Id FROM Steps s INNER JOIN Recipes r ON r.recipe_Id=s.recipe_Id WHERE r.recipe_Id=? AND step_Id=?;`,
-      recipeId,
-      stepId,
-      (err, row) => {
-        if (err) return res.json({ status: 300, success: false, error: err });
-        data.push({ stepId: row.step_Id, text: row.step_detail });
-      },
-      () => {
-        res.send(data);
-      }
-    );
-  });
+  if (req.cookies.usertype === "premium" || req.cookies.usertype === "admin") {
+    db.serialize(() => {
+      db.each(
+        `SELECT s.step_detail,s.step_Id FROM Steps s INNER JOIN Recipes r ON r.recipe_Id=s.recipe_Id WHERE r.recipe_Id=? AND step_Id=?;`,
+        recipeId,
+        stepId,
+        (err, row) => {
+          if (err) return res.json({ status: 300, success: false, error: err });
+          data.push({ stepId: row.step_Id, text: row.step_detail });
+        },
+        () => {
+          res.send(data);
+        }
+      );
+    });
+  } else {
+    db.serialize(() => {
+      db.each(
+        `SELECT s.step_detail,s.step_Id FROM Steps s INNER JOIN Recipes r ON r.recipe_Id=s.recipe_Id WHERE r.recipe_Id=? AND step_Id=? AND r.category="Free";`,
+        recipeId,
+        stepId,
+        (err, row) => {
+          if (err) return res.json({ status: 300, success: false, error: err });
+          data.push({ stepId: row.step_Id, text: row.step_detail });
+        },
+        () => {
+          if (data.length < 1)
+            return res.json({
+              status: 401,
+              success: false,
+              error: "Unauthorized user, get a premium user token to continue",
+            });
+          return res.json({ status: 200, data: data, success: true });
+        }
+      );
+    });
+  }
 });
 
 // PREMIUM TIER
@@ -173,9 +257,8 @@ app.get("/recipe/:recipe_Id/:step_Id", (req, res) => {
 // Get all recipes that have a given ingredient
 app.get("/search/:ingredient", (req, res) => {
   let ingredientType = req.params.ingredient;
-  // console.log(req.cookies.usertype);
   let data = [];
-  if (req.cookies.usertype === "premium") {
+  if (req.cookies.usertype === "premium" || req.cookies.usertype === "admin") {
     db.serialize(() => {
       db.each(
         "Select r.recipe_Name, r.recipe_Id, i.ingredient_Type from Recipes r inner JOIN RecipeIngredients t on r.recipe_Id= t.recipe_Id inner join Ingredients i on t.ingredient_Id = i.ingredient_Id WHERE i.ingredient_Type LIKE ?;",
@@ -191,7 +274,7 @@ app.get("/search/:ingredient", (req, res) => {
     });
   } else {
     return res.json({
-      status: 401,
+      status: 403,
       message:
         "You are not authorized to access this, become a Premium user to get access",
       success: false,
@@ -201,9 +284,8 @@ app.get("/search/:ingredient", (req, res) => {
 
 // Get list af all the ingredients available in database
 app.get("/ingredients", (req, res) => {
-  // console.log(req.body);
   let data = [];
-  if (req.cookies.usertype === "premium") {
+  if (req.cookies.usertype === "premium" || req.cookies.usertype === "admin") {
     db.serialize(() => {
       db.each(
         "SELECT ingredient_Type FROM Ingredients ",
@@ -218,7 +300,7 @@ app.get("/ingredients", (req, res) => {
     });
   } else {
     return res.json({
-      status: 401,
+      status: 403,
       message:
         "You are not authorized to access this, become a Premium user to get access",
       success: false,
@@ -237,103 +319,95 @@ app.get("/auth", (req, res) => {
 // ADD A NEW RECIPE
 
 app.post("/recipe", (req, res) => {
-  let data;
-  // if (req.cookies.usertype === "admin") {
-  const { name, category, ingredients, stepCount, steps } = req.body;
-
-  data = {
-    name: name,
-    category: category,
-    ingredients: ingredients,
-    stepCount: stepCount,
-    steps: steps,
-  };
-  let recipeName = name;
-  let categoryName = category;
-  let IngredientType = ingredients[0].type;
-  let MeasureDetail = ingredients[0].entry;
-  let stepNumber = steps[0].step_id;
-  let stepText = steps[0].text;
-
-  db.serialize(() => {
-    db.each(
-      `INSERT INTO  Recipes (recipe_Name, step_Count, category) VALUES (?,?, ?)`,
-      recipeName,
-      stepCount,
-      categoryName
-    ),
+  if (req.cookies.usertype === "admin") {
+    const { name, category, ingredients, steps } = req.body;
+    let recipeName = name;
+    let categoryName = category;
+    let stepCount = steps.length;
+    db.serialize(() => {
       db.each(
-        "SELECT recipe_Id FROM Recipes WHERE recipe_Name = ? ",
+        `INSERT INTO  Recipes (recipe_Name, step_Count, category) VALUES (?,?, ?)`,
         recipeName,
-        (err, row) => {
-          if (err) {
-            res.status(404).json({ Error: "An error occured" });
-          }
-          let id = row.recipe_Id;
-          for (let i = 0; i < steps.length; i++) {
-            db.each(
-              `INSERT INTO Steps ( step_Id,recipe_Id,step_detail) VALUES (?,?,?)`,
-              steps[i].step_id,
-              id,
-              steps[i].text
-            );
-          }
-          for (let i = 0; i < ingredients.length; i++) {
-            db.each(
-              `INSERT OR IGNORE INTO Ingredients (ingredient_Type) VALUES(?)`,
-              ingredients[i].type
-            ),
+        stepCount,
+        categoryName
+      ),
+        db.each(
+          "SELECT recipe_Id FROM Recipes WHERE recipe_Name = ? ",
+          recipeName,
+          (err, row) => {
+            if (err) {
+              res.status(404).json({ Error: "An error occured" });
+            }
+            let id = row.recipe_Id;
+            for (let i = 0; i < steps.length; i++) {
               db.each(
-                "SELECT ingredient_Id FROM Ingredients WHERE ingredient_Type = ?",
-                ingredients[i].type,
-                (err, row) => {
-                  if (err) {
-                    res.status(404).json({
-                      Error: err.message,
-                    });
-                  }
-                  let ingredientId = row.ingredient_Id;
-                  console.log("Ingredient id inside loop ", ingredientId);
-                  db.run(
-                    `INSERT INTO RecipeIngredients (recipe_Id, ingredient_Id) VALUES (?, ?)`,
-                    id,
-                    ingredientId
-                  );
-
-                  db.run(
-                    `INSERT INTO Measurements (measure,recipe_Id) VALUES (?, ?)`,
-                    ingredients[i].entry,
-                    id
-                  ),
-                    db.each(
-                      "SELECT measure_Id FROM Measurements WHERE recipe_Id = ?",
-                      id,
-                      (err, row) => {
-                        if (err) {
-                          res.status(404).json({
-                            Error: err.message,
-                          });
-                        }
-                        let measureId = row.measure_Id;
-                        db.run(
-                          `UPDATE RecipeIngredients SET measure_Id =${measureId}  WHERE recipe_Id =(?) AND ingredient_Id = (?)`,
-                          id,
-                          ingredientId
-                        );
-                      }
-                    );
-                }
+                `INSERT INTO Steps ( step_Id,recipe_Id,step_detail) VALUES (?,?,?)`,
+                steps[i].step_id,
+                id,
+                steps[i].text
               );
-          }
-        },
-        () => {
-          res.send(data);
-        }
-      );
-  });
-  // }
+            }
+            for (let j = 0; j < ingredients.length; j++) {
+              db.each(
+                `INSERT OR IGNORE INTO Ingredients (ingredient_Type) VALUES(?)`,
+                ingredients[j].type
+              ),
+                db.each(
+                  "SELECT ingredient_Id FROM Ingredients WHERE ingredient_Type = ?",
+                  ingredients[j].type,
+                  (err, row) => {
+                    if (err) {
+                      res.status(404).json({
+                        Error: err.message,
+                      });
+                    }
+                    let ingredientId = row.ingredient_Id;
+                    console.log("Ingredient id inside loop ", ingredientId);
+                    db.run(
+                      `INSERT INTO RecipeIngredients (recipe_Id, ingredient_Id) VALUES (?, ?)`,
+                      id,
+                      ingredientId
+                    );
 
-  // res.send("recipe saved"););
+                    db.run(
+                      `INSERT INTO Measurements (measure,recipe_Id) VALUES (?, ?)`,
+                      ingredients[j].entry,
+                      id
+                    ),
+                      db.each(
+                        "SELECT measure_Id FROM Measurements WHERE recipe_Id = ?",
+                        id,
+                        (err, row) => {
+                          if (err) {
+                            res.status(404).json({
+                              Error: err.message,
+                            });
+                          }
+                          let measureId = row.measure_Id;
+                          db.run(
+                            `UPDATE RecipeIngredients SET measure_Id =${measureId}  WHERE recipe_Id =(?) AND ingredient_Id = (?)`,
+                            id,
+                            ingredientId
+                          );
+                        }
+                      );
+                  }
+                );
+            }
+          },
+          () => {
+            res.send("Recipe saved successfully");
+          }
+        );
+    });
+  } else {
+    return res.json({
+      status: 403,
+      message:
+        "You are not authorized to access this, become a Premium user to get access",
+      success: false,
+    });
+  }
 });
 
 // Update Recipe
@@ -345,81 +419,55 @@ app.put("/recipe/:recipe_Id", (req, res) => {});
 // Delete Recipe
 
 app.delete("/recipe/:recipe_Id", (req, res) => {
-  console.log(req.params.recipe_Id);
-  let deletedRecord;
-  db.serialize(() => {
-    db.each(
-      `DELETE from Recipes WHERE recipe_Id = ?`,
-      req.params.recipe_Id,
-      (err, res) => {
-        if (err) return res.json({ status: 300, success: false, error: err });
-        deletedRecord = res;
-      }
-    );
-    db.each(
-      `DELETE from Steps WHERE recipe_Id = ?`,
-      req.params.recipe_Id,
-      (err, res) => {
-        if (err) return res.json({ status: 300, success: false, error: err });
-        deletedRecord = res;
-      }
-    );
-    db.each(
-      `DELETE from RecipeIngredients WHERE recipe_Id = ?`,
-      req.params.recipe_Id,
-      (err, res) => {
-        if (err) return res.json({ status: 300, success: false, error: err });
-        deletedRecord = res;
-      }
-    );
-    db.each(
-      `DELETE from Measurements WHERE recipe_Id = ?`,
-      req.params.recipe_Id,
-      (err, res) => {
-        if (err) return res.json({ status: 300, success: false, error: err });
-        deletedRecord = res;
-      },
-      () => {
-        res.send("record deleted");
-      }
-    );
-  });
+  if (req.cookies.usertype === "admin") {
+    db.serialize(() => {
+      db.each(
+        `DELETE from Recipes WHERE recipe_Id = ?`,
+        req.params.recipe_Id,
+        (err, res) => {
+          if (err) return res.json({ status: 300, success: false, error: err });
+          deletedRecord = res;
+        }
+      );
+      db.each(
+        `DELETE from Steps WHERE recipe_Id = ?`,
+        req.params.recipe_Id,
+        (err, res) => {
+          if (err) return res.json({ status: 300, success: false, error: err });
+          deletedRecord = res;
+        }
+      );
+      db.each(
+        `DELETE from RecipeIngredients WHERE recipe_Id = ?`,
+        req.params.recipe_Id,
+        (err, res) => {
+          if (err) return res.json({ status: 300, success: false, error: err });
+          deletedRecord = res;
+        }
+      );
+      db.each(
+        `DELETE from Measurements WHERE recipe_Id = ?`,
+        req.params.recipe_Id,
+        (err, res) => {
+          if (err) return res.json({ status: 300, success: false, error: err });
+          deletedRecord = res;
+        },
+        () => {
+          res.send("record deleted");
+        }
+      );
+    });
+  } else {
+    return res.json({
+      status: 403,
+      message:
+        "You are not authorized to access this, become a Premium user to get access",
+      success: false,
+    });
+  }
 });
 
 // Server listening
 app.listen(port, () => {
   console.log(`server is running at http://${hostname}:${port}`);
 });
-
-// .each(`SELECT MAX(recipe_Id) AS id FROM Recipes`)
-// .run(
-//   `INSERT INTO Ingredients (ingredient_Type) VALUES(?)`,
-//   ingredients.type
-// )
-// .run(
-//   `INSERT INTO Measurements (measure,recipe_Id) VALUES (?, ?)`,
-//   ingredients.entry,
-//   id
-// );
-// db.run(`SELECT * from Recipes`);
-
-// let id = db.lastInsertId();
-// let id = db.run(`SELECT last_insert_rowid();`);
-// SELECT MAX(rowid) FROM your_table_name
-// console.log("id", id);
-// db.run(`SELECT MAX(recipe_Id) As id FROM Recipes`);
-// db.run(
-//   `INSERT INTO Ingredients (ingredient_Type) VALUES(?)`,
-//   ingredients.type
-// );
-// db.run(
-//   `INSERT INTO Measurements (measure,recipe_Id) VALUES (?, ?)`,
-//   ingredients.entry,
-//   id
-// );
-// db.run(
-//   `INSERT INTO Steps (step_Id, step_detail,recipe_Id) VALUES (?,?,?)`,
-//   steps.step_id,
-//   steps.text,
-//   id
-// );
